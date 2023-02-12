ee502faa8b613198f0338a0b3ccc1d773350878b
orenji <katching.pei@gmail.com>
2023-02-12 09:26:59 +0900

    changed env var

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 APPLICABLE_RATE_TYPES = {"JPY":"🇯🇵", "AUD":"🇦🇺", "USD":"🇺🇸", "SGD":"🇸🇬", "CAD": "🇨🇦", "EUR": "🇪🇺"}
 
 ACCOUNT = int(os.environ["ACCOUNT"])
-BACKEND_URL = os.environ["BACKEND_URL"]
+BACKEND_URL = "http://backend:{}/".format(os.environ["BACKEND_PORT"])
 TWITTER_BEARER_KEY = os.environ["TWITTER_BEARER_KEY"]
